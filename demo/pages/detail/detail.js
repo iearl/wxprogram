b808e87eb9d89199ab5e122085419f4b19ff504b
iearl <dc_magaowei@163.com>
2019-09-16 23:41:05 +0800

    微信详情+页面缓存+音乐播放

--- demo/pages/detail/detail.js
+++ demo/pages/detail/detail.js
@@ -1,66 +1,81 @@
 // pages/detail/detail.js
+let datas = require('../../datas/list-data');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    detailObj:{},
+    index:null,
+    isCollectioned:false,
+    isMuicPlay:false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    let index = options.index;
+    //更新detailObj值
+    this.setData({
+      detailObj : datas.list_data[index],
+      index
+    });
+    //根据本地缓存的数据判断用户是否收藏当前文章
+    let detailStorage = wx.getStorageSync('isCollectioned');
 
+    if (!detailStorage){
+      wx.setStorageSync('isCollectioned', {});
+    }
+    //判断用户是否收藏
+    if(detailStorage[index]){
+      this.setData({
+      })
+    }
   },
+  handlerCollectiom(){
+    let isCollectioned = !this.data.isCollectioned;
+    this.setData({
+      isCollectioned
+    });
+    //提示用户
+    let title = isCollectioned?'收藏成功':'取消收藏';
+    wx.showToast({
+      title: title,
+      icon:'success'
+    });
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+    //缓存数据到本地
+    let {index} = this.data;
+    wx.getStorage({
+      key: 'isCollectioned',
+      success:(datas)=> {
+        console.log(datas,typeof datas);
+        let obj = datas.data;
+        obj[index] = isCollectioned;
+        wx.setStorage({
+          key: 'isCollectioned',
+          data: obj,
+        })
+      }
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handlerMusic(){
+    //处理音乐播放
+    let isMuicPlay = !this.data.isMuicPlay;
+    this.setData({
+      isMuicPlay
+    });
+    //控制音乐播放
+    if(isMuicPlay){
+      let {dataUrl,title} = this.data.detailObj.music;
+      wx.playBackgroundAudio({
+        dataUrl: dataUrl,
+        title: title
+      })
+    }else{
+      wx.pauseBackgroundAudio();
+    }
   }
 })
--- demo/pages/list/list.js
+++ demo/pages/list/list.js
@@ -16,7 +16,25 @@ Page({
   onLoad: function (options) {
     this.setData({ listArr: datas.list_data });
   },
-
+  toDetail(event){
+    let index = event.currentTarget.dataset.index;
+    console.log(index);
+    //wx.redirectTo({ 当前页卸载
+    //点击跳转到详情页 保留当前页
+    wx.navigateTo({
+      url: '/pages/detail/detail?index='+index,
+    })
+  },
+  //点击轮播图显示详情页
+  swiperDetailTo(event){
+    let index = event.target.dataset.index;
+    console.log(index);
+    //wx.redirectTo({ 当前页卸载
+    //点击跳转到详情页 保留当前页
+    wx.navigateTo({
+      url: '/pages/detail/detail?index=' + index,
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
